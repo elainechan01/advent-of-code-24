0a09daeef5ab14d1ec440fefc57c35c033cb3c8e
elainechan01 <elaineyunru.chan@wmich.edu>
2024-12-02 00:57:36 -0500

    d1 part2

--- d1/hh.py
+++ d1/hh.py
@@ -4,6 +4,7 @@ Date: 12/2/24
 Program: Advent of Code Day 1 - Historian Hysteria
 """
 import bisect
+from collections import Counter
 
 class HystorianHysteria:
     def __init__(self) -> None:
@@ -16,6 +17,11 @@ class HystorianHysteria:
             bisect.insort(self.list1, ID1)
             bisect.insort(self.list2, ID2)
     
-    def calculateSumOfDist(self) -> int:
+    def calcSumOfDist(self) -> int:
         res = sum([abs(int(self.list1[i]) - int(self.list2[i])) for i in range(len(self.list1))])
+        return res
+    
+    def calcSimilarityScore(self) -> int:
+        list2_frequency = Counter(self.list2)
+        res = sum([int(num) * list2_frequency[num] for num in self.list1])
         return res
